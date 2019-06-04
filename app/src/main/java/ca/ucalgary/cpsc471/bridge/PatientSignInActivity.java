2a2b3584a61adeba446701d8ec1ba4da4ccdeef2
Raymond Tran <traymondbiz@gmail.com>
2019-06-03 22:30:34 -0600

    Added a basic case for patient sign-ins. (ID = 1)
    - Takes the user to a 'view appointments' screen by default.

--- app/src/main/java/ca/ucalgary/cpsc471/bridge/PatientSignInActivity.java
+++ app/src/main/java/ca/ucalgary/cpsc471/bridge/PatientSignInActivity.java
@@ -1,7 +1,11 @@
 package ca.ucalgary.cpsc471.bridge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PatientSignInActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class PatientSignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_in);
+    }
+
+    public void attemptSignIn(View view){
+        EditText inputIDField = (EditText) findViewById(R.id.editText2);
+        try {
+            int inputID = Integer.parseInt(inputIDField.getText().toString());
+            if (inputID != 1){
+                // TODO: Replace with query to DB. Should check to see if valid ID. If so, populate fields with provided ID.
+                throw new NumberFormatException();
+            }
+            else {
+                // TODO: Send the valid ID to the next activity. (View Appointments by default?) So that it can propagate that information.
+                Intent intent = new Intent(this, PatientViewAppointmentsActivity.class);
+                startActivity(intent);
+
+            }
+        }
+        catch (NumberFormatException e){
+            Toast.makeText(PatientSignInActivity.this, "ERROR: Invalid ID. (Try '1')", Toast.LENGTH_LONG).show();
+        }
     }
 }
